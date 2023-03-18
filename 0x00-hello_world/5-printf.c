ca7c80b80dc0ad72e7cd81acb5840e87c0a3e730
mugishab2020 <mugishab2020@gmail.com>
2023-03-18 12:57:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - function that print the line with printf function
- * Return:0(success)
+ * main - function that print the line with printf f unction
+ * Return: 0(success)
   */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
