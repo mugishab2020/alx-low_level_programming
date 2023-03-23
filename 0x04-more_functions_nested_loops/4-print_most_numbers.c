17cdeac06b9179e12560dab8e47f32ec15127623
mugishab2020 <mugishab2020@gmail.com>
2023-03-23 15:43:41 -0700

    the one that is taff

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,11 @@
 void print_most_numbers(void)
 {
 char c;
-for(c='0';c<='9';c++)
+for (c = '0'; c <= '9'; c++)
 {
-if(!(c == '2' || c == '4')){
-	_putchar(c);
+if (!(c == '2' || c == '4'))
+{
+	_putchar(c+'0');
 }
 }
 _putchar('\n');
