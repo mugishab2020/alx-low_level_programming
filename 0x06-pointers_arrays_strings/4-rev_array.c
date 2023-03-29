8d53c3d72d70c892b552f3826f33f39f3bf39cab
mugishab2020 <mugishab2020@gmail.com>
2023-03-29 11:06:16 -0700

    Always look up

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,11 @@
  */
 void reverse_array(int *a, int n)
 {
-int i;
-int temp;
-for (i = 0; i < n; i++)
+int temp, i;
+for (i = 0; i < n--; i++)
 {
-	temp = a[i];
-	a[i] = a[n];
-	a[n] = temp;
-	n--;
-}
+        temp = a[i];
+        a[i] = a[n];
+        a[n] = temp;
+       }
 }
