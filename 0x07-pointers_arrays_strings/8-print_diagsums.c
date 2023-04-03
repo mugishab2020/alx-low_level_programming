10ce2a22118473b52d7f7e34134f07e4f795c94c
mugishab2020 <mugishab2020@gmail.com>
2023-04-03 10:58:27 -0700

    8-print_diagsums"

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums -  function entry point
+ * @b: the input array
+ * @size: the size of input array
+ * Return: alwys ) (as success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int rsum, lsum;
+	int b;
+
+	rsum = 0;
+	lsum = 0;
+	for (b = 0; b < size; b++)
+	{
+		rsum = rsum + a[b * size + b];
+
+
+	}
+	for (b = size; b >= 0; b--)
+	{
+	lsum = lsum + a[b * size + (size - b - 1)];
+	}
+	printf("%d, %d\n", rsum, lsum);
+}
