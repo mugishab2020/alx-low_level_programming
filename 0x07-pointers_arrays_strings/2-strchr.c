058537a9bd383fe40651f20b48bd228290949107
mugishab2020 <mugishab2020@gmail.com>
2023-04-03 04:35:11 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strchr - Entry point
+ * @s: The arrray/point to print
+ * @c: The characher to start from
+ * Return: The array stord in s
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	a = 0;
+while (s[a] >= '\0')
+{
+	if (s[a] == c)
+	{
+		return (&s[a]);
+	}
+}
+/*
+ *for (a = 0; s[a] >= '\0'; a++)
+ *if (s[a] == c)		{
+*return (&s[a]);
+*		}
+*/
+	return (0);
+}
