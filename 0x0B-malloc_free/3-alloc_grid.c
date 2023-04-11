c308d814a77e47156812d9ea4213e3b48d6336c2
mugishab2020 <mugishab2020@gmail.com>
2023-04-11 07:31:29 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - the function to returnn 2-d arry
+ * @width: the first number index
+ * @height: the second number of index
+ * Return: the point of 2D
+ */
+int **alloc_grid(int width, int height)
+{
+int **arr;
+int a, b;
+
+if (width <= 0 || height <= 0)
+	return (NULL);
+arr = malloc(height * sizeof(int *));
+if (arr == NULL)
+	return (NULL);
+for (a = 0; a < height; a++)
+{
+	arr[a] = malloc(sizeof(int) * width);
+	if (arr[a] == NULL)
+	{
+		for (; a > 0; a--)
+			free(arr[a]);
+
+
+	free(arr);
+	return (NULL);
+}
+}
+for (a = 0; a < height; a++)
+{
+for (b = 0; b < width; b++)
+{
+	arr[a][b] = 0;
+}
+}
+return (arr);
+}
