2bff95c3d022652af14f7f366ceeb7dd5d44a262
mugishab2020 <mugishab2020@gmail.com>
2023-04-11 07:38:02 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,11 +11,11 @@ int **alloc_grid(int width, int height)
 int **arr;
 int a, b;
 
-if (width <= 0 || height <= 0)
-	return (NULL);
 arr = malloc(height * sizeof(int *));
-if (arr == NULL)
+
+if (arr == NULL || width <= 0 || height <= 0)
 	return (NULL);
+
 for (a = 0; a < height; a++)
 {
 	arr[a] = malloc(sizeof(int) * width);
