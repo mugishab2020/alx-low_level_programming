189f612fb1091885da05f7ecdeb4840c926f69ac
mugishab2020 <mugishab2020@gmail.com>
2023-04-17 07:46:00 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - man entry of the program
+ * FILE: the name of the file
+ *Return: the name of the file
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+	return (0);
+}
