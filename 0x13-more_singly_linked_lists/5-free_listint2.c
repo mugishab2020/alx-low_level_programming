d46c3905e12ec3575ac5dd8aaaabed552fa0f8cf
mugishab2020 <mugishab2020@gmail.com>
2023-04-28 08:21:50 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - function to free whole listt;
+ * @head: pointer to the head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tempo;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tempo = (*head)->next;
+		free(*head);
+		*head = tempo;
+
+	}
+	*head = NULL;
+}
