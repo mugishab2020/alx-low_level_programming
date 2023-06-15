1652869501acf6d5da3de6d914c001c831aca7e7
mugishab2020 <mugishab2020@gmail.com>
2023-06-15 02:49:21 -0700

    The first task is almost done

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - fu8nction for printing dlistint_t element
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int number;
+
+	number = 0;
+
+	if (h == NULL)
+		return (number);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+0x17. C - Doubly linked lists
+this is the addiitional project of linked ist and it deals with the double linked list and how to use them in c 
