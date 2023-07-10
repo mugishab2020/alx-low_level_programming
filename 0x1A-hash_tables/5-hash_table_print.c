0c9856b5bfa595d8d4dfcb541fd0297a480a10e9
mugishab2020 <mugishab2020@gmail.com>
2023-07-10 13:04:40 -0700

    now we are going

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - functioon to print hash table.
+ * @ht: the hash table to print
+ */
+ void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	unsigned char flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			node = ht->array[index];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
