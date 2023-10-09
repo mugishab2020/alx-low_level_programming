d011fbb461cd56383be2ba3bb5495d2760f1d6f9
mugishab2020 <mugishab2020@gmail.com>
2023-10-09 04:35:23 -0700

    linear searching  is understood

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - fn that make the binary search
+ * @array: the array to search into
+ * @size: ths size of the array
+ * @value: the value top search into the array
+ * Return: index of value or Null or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t a = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (; a < size; a++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		if (array[a] == value)
+			return ((int) a);
+
+	}
+	return (-1);
+}
