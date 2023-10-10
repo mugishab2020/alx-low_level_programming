41cd23a6776344396bdb8df54869011949b9be0f
mugishab2020 <mugishab2020@gmail.com>
2023-10-10 02:19:40 -0700

    modifying

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 		if (array[a] == value)
-			return ((int) a);
+			return (a);
 
 	}
 	return (-1);
