f401263caa7aa633c77787e60253335c508982fc
mugishab2020 <mugishab2020@gmail.com>
2023-10-11 04:23:12 -0700

    Interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,28 @@
+#include"search_algos.h"
+/**
+ * interpolation_search - the function to implement tnterpolating search
+ * @array: the array to search from
+ * @size: the size of the array to search from
+ * @value: the key to be searched
+ * Return: the first index for the key to be found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t h = size - 1, l = 0, p;
+
+	if (array == NULL)
+		return (-1);
+	while (l <= h && value >= array[l] && value <= array[h])
+	{
+		p = l + (((h - l) / (array[h] - array[l])) * (value - array[l]));
+		printf("Value checked array[%lu] = [%d]\n", p, array[p]);
+		if (array[p] == value)
+			return (p);
+		else if (array[p] < value)
+			l = p + 1;
+		else
+			h = p - 1;
+	}
+	printf("Value checked array[%ld] is out of range\n", p);
+	return (-1);
+}
